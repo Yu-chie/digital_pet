4b8a078c1613f1b5ccc39f435d6db2594850d444
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-06-23 19:09:43 +0800

    Add Function for Loading screen

--- digital_pet.py
+++ digital_pet.py
@@ -204,7 +204,13 @@ Mood: {self.mood()}
         )
 
 
-# Step 13: Implement loading_screen() function to simulate loading animation on startup
+    # Function for loading screen
+    def loading_screen():
+        print("👶 A new baby pet is being born...")
+        for i in range(3):
+            time.sleep(1)
+            print("Loading" + "." * (i + 1))
+        print("✨ Your DigiPet is here!\n")
 
 # Step 14: Implement main() function to:
 # - Load existing pet or create new pet
